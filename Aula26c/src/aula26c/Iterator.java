2fb8d9340fd3cf2e50e389e74214b79bae2983b6
Arnald Souza <87627213+ArnaldSouza@users.noreply.github.com>
2023-08-31 16:17:03 -0300

    Delete Aula26c directory

--- Aula26c/src/aula26c/Iterator.java
+++
@@ -1,9 +0,0 @@
-
-package aula26c;
-
-public interface Iterator {
-    public abstract boolean temProximo();
-    public abstract Object proximo();
-    public abstract boolean temAnterior();
-    public abstract Object  anterior();
-}
